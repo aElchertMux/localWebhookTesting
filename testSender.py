59e6b17c9b997e30a1a20cf2cdabfbff76b23980
Adam Elchert <87077207+aElchertMux@users.noreply.github.com>
2022-04-15 14:50:35 -0400

    auto check for testSender

--- testSender.py
+++ testSender.py
@@ -1,7 +1,12 @@
-import requests, json
+import requests, json, os
 
-webhookURL = ''
+# the ngrok server must be running to catch the webhookURL
+rawData = requests.get("http://127.0.0.1:4040/api/tunnels")
+r_unicode = rawData.content.decode("utf-8")
+r_json = json.loads(r_unicode)
+webhookURL = r_json['tunnels'][0]['public_url']
 
+# sample webhook body
 data = {'name': 'adam', 'dogs': ['conway', 'hank']}
 
 r = requests.post(webhookURL, data=json.dumps(data), headers={'Content-type': 'application/json'})
